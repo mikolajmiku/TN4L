854adead49f95b9e95147eeb757b6f9546d50abf
admahm <admahm@softserveinc.com>
2023-08-18 16:17:00 +0200

    add new Text

--- components/Home/OurPartners.tsx
+++ components/Home/OurPartners.tsx
@@ -1,6 +1,30 @@
-import { Container, Flex, Heading, Text } from "@chakra-ui/react";
+import { Container, Flex, Heading } from "@chakra-ui/react";
 
 import Carousel from "../Carousel";
+
+//TODO: Replace this with Richtext from Strapi CMS
+const Richtext = () => (
+  <>
+    Współpracujemy z organizacjami, firmami i osobami, którym bliska jest praca
+    ze zwierzętami i w naturze.
+    <br />
+    „W pojedynkę możemy niewiele; razem możemy tak wiele.” (Helen Keller). Tak
+    właśnie jest - dlatego współpracujemy z organizacjami, firmami i osobami,
+    którym bliska jest praca ze zwierzętami i w naturze. Wśród naszych partnerów
+    są:
+    <br /> <br />
+    <ul>
+      <li>Fundacja Czarodziejska Góra</li>
+      <li>Mar-Tusia - hipoterapia i rehabilitacja dla dzieci</li>
+      <li>BejRoots - hipo&fizjoterapia</li>
+      <li>Amigos - zagroda Alpak</li>
+      <li>Stajnia Zadora</li>
+      <li>Miejsce Rozwoju Anna Łaszcz</li>
+      <li>Pracownia Relacje</li>
+      <li>Katarzyna Kabza i pies terapeutyczny Miko</li>
+    </ul>
+  </>
+);
 
 const OurPartners = () => (
   <Flex
@@ -12,11 +36,7 @@ const OurPartners = () => (
     <Heading as="h1">Nasi Partnerzy</Heading>
     {/* Citation Card */}
     <Container maxW="3xl">
-      <Text>
-        W zgodzie z tą myślą tworzymy programy zajęć przy współpracy z
-        organizacjami, firmami i osobami, którym bliska jest praca ze
-        zwierzętami i w naturze.
-      </Text>
+      <Richtext />
     </Container>
     <Carousel />
   </Flex>
