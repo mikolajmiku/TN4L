20c4679926c40260c7019440f84f7ead8dd66413
admahm <admahm@softserveinc.com>
2023-08-18 12:48:41 +0200

    new slider package

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,13 +1,10 @@
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/virtual";
-
 import { Box } from "@chakra-ui/react";
-import { Virtual } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
+import chakraTheme from "@chakra-ui/theme";
+import { useState } from "react";
+import ReactSimplyCarousel from "react-simply-carousel";
 
+import ArrowButton from "./ArrowButton";
 import { PartnerLogo } from "./PartnerLogo";
-
 export const partnerLogos = [
   {
     img: "images/partner-logos/miejsce_rozwoju_logo.jpg",
@@ -25,24 +22,49 @@ export const partnerLogos = [
     img: "images/partner-logos/amigos_logo.png",
     title: "Amigos",
   },
+  {
+    img: "images/partner-logos/pracownia_logo.png",
+    title: "Pracownia Relacje",
+  },
 ];
 
-const Carousel = () => (
-  <Box width="100%">
-    <Swiper
-      modules={[Virtual]}
-      spaceBetween={176}
-      slidesPerView={3}
-      centeredSlides={true}
-      virtual
-    >
-      {partnerLogos.map((partnerLogo, index) => (
-        <SwiperSlide key={index}>
-          <PartnerLogo img={partnerLogo.img} title={partnerLogo.title} />
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  </Box>
-);
+const Carousel = () => {
+  const [activeSlideIndex, setActiveSlideIndex] = useState(0);
+  return (
+    <Box width="100%">
+      <ReactSimplyCarousel
+        centerMode={true}
+        activeSlideIndex={activeSlideIndex}
+        onRequestChange={setActiveSlideIndex}
+        containerProps={{
+          style: {
+            flexWrap: "nowrap",
+            justifyItems: "stretch",
+            gap: chakraTheme.sizes[6],
+          },
+        }}
+        backwardBtnProps={{
+          children: <ArrowButton />,
+        }}
+        forwardBtnProps={{
+          children: <ArrowButton rotate={true} />,
+        }}
+        innerProps={{
+          style: {
+            maxWidth: chakraTheme.sizes.container.xl,
+          },
+        }}
+      >
+        {partnerLogos.map((partnerLogo, index) => (
+          <PartnerLogo
+            key={index}
+            img={partnerLogo.img}
+            title={partnerLogo.title}
+          />
+        ))}
+      </ReactSimplyCarousel>
+    </Box>
+  );
+};
 
 export default Carousel;
